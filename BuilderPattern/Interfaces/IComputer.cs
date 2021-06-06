2fcfefc90c8577f049b23b5ce9f0854ba66b716c
Maksym Fedorenko <m.fedorenko@upswot.com>
2021-06-06 23:21:21 +0300

    [Add] Builder Pattern

---
+++ BuilderPattern/Interfaces/IComputer.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace BuilderPattern.Interfaces
+{
+    public interface IComputer
+    {
+        public bool TurnOnOrOff();
+
+        public void PlayGame();
+
+        public void DoHomework();
+
+        public void Display(string str);
+    }
+}
